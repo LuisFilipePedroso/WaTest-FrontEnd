7872d645cc159a119edd801d3e1d5e06b0ddbfc3
Luis Filipe <luis_filipe42@outlook.com>
2019-11-18 08:55:32 -0300

    The project was finalized

---
+++ src/services/order.ts
@@ -0,0 +1,16 @@
+import IOrder from 'interfaces/models/order';
+import { IPaginationParams, IPaginationResponse } from 'interfaces/pagination';
+import { Observable } from 'rxjs';
+
+import apiService, { ApiService } from './api';
+
+export class OrderService {
+  constructor(private apiService: ApiService) {}
+
+  public list(params: IPaginationParams): Observable<IPaginationResponse<IOrder>> {
+    return this.apiService.get('/order', params);
+  }
+}
+
+const orderService = new OrderService(apiService);
+export default orderService;
